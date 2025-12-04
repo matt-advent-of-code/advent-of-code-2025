56bba91cb075b74b7d5496be9a1df25886d4598a
matthew-js-porter <matthew.js.porter@jpmchase.com>
2025-12-04 09:02:17 -0500

    day 4 part 2

---
+++ day4/part2.py
@@ -0,0 +1,59 @@
+def parse(diagram: str) -> [[str]]:
+    rolls = []
+    for line in diagram.splitlines():
+        rolls.append(list(line))
+    return rolls
+
+def is_accessible(rolls: [[str]], x: int, y: int) -> bool:
+    if rolls[x][y] != '@':
+        return False
+    neighbor_count = 0
+    if x + 1 < len(rolls):
+        if rolls[x + 1][y] == '@':
+            neighbor_count += 1
+        if y + 1 < len(rolls[x + 1]) and rolls[x + 1][y + 1] == '@':
+            neighbor_count += 1
+        if y - 1 >= 0 and rolls[x + 1][y - 1] == '@':
+            neighbor_count +=1
+    if x - 1 >= 0:
+        if rolls[x - 1][y] == '@':
+            neighbor_count += 1
+        if y + 1 < len(rolls[x - 1]) and rolls[x - 1][y + 1] == '@':
+            neighbor_count += 1
+        if y - 1 >= 0 and rolls[x - 1][y - 1] == '@':
+            neighbor_count +=1
+
+    if y + 1 < len(rolls[x]) and rolls[x][y + 1] == '@':
+        neighbor_count += 1
+    if y - 1 >= 0 and rolls[x][y - 1] == '@':
+        neighbor_count += 1
+
+    return neighbor_count < 4
+
+def count_accessible(rolls: [[str]]) -> int:
+    accessible = 0
+    count = count_and_remove(rolls)
+    while count != 0:
+        accessible += count
+        count = count_and_remove(rolls)
+    return accessible
+
+
+def count_and_remove(rolls: [[str]]) -> int:
+    accessible = 0
+    to_remove : [(int, int)] = []
+    for x in range(0, len(rolls)):
+        for y in range(0, len(rolls[x])):
+            is_paper_accessible = is_accessible(rolls, x, y)
+            accessible += is_paper_accessible
+            if is_paper_accessible:
+                to_remove.append((x,y))
+    for r in to_remove:
+        rolls[r[0]][r[1]] = '.'
+    return accessible
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        rolls = parse(f.read())
+        print(count_accessible(rolls))
---
+++ day4/test_part2.py
@@ -0,0 +1,60 @@
+import unittest
+
+from day4.part2 import parse, is_accessible, count_accessible
+
+
+class MyTestCase(unittest.TestCase):
+    def test_parse(self):
+        rolls = parse(""".@
+@.
+""")
+        expected = [['.', '@'],
+                    ['@', '.']]
+        self.assertEqual(expected, rolls)  # add assertion here
+
+    def test_not_roll(self):
+        rolls = [['.', '@'],
+                ['@', '.']]
+        self.assertFalse(is_accessible(rolls, 0, 0))
+
+    def test_top_right(self):
+        rolls = [['.', '@'],
+                ['@', '.']]
+        self.assertTrue(is_accessible(rolls, 0, 1))
+
+    def test_bottom_let(self):
+        rolls = [['.', '@'],
+                ['@', '.']]
+        self.assertTrue(is_accessible(rolls, 1, 0))
+
+
+    def test_middle(self):
+        rolls = [['.', '@', '.'],
+                 ['@', '@', '.'],
+                 ['@', '.', '.']]
+        self.assertTrue(is_accessible(rolls, 1, 1))
+
+    def test_not_accessible(self):
+        rolls = [['.', '@', '.'],
+                 ['@', '@', '.'],
+                 ['@', '@', '.']]
+        self.assertFalse(is_accessible(rolls, 1, 1))
+
+    def test_puzzle(self):
+        puzzle_input = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+        rolls = parse(puzzle_input)
+        self.assertEqual(43, count_accessible(rolls))
+
+
+if __name__ == '__main__':
+    unittest.main()
